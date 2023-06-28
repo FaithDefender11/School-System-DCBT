5a0d8ccfe70e5b94215a8b3a2abbebe4fbcd920f
IrisLibre32 <dhanmanaog@gmail.com>
2023-06-28 12:37:34 +0800

    Update index.php

--- admin/section/index.php
+++ admin/section/index.php
@@ -2,7 +2,75 @@
 
 <?php include_once('../../includes/admin_header.php')?>
 
-<h4>This is Section Index</h4>
+    <div class="content">
+      <nav>Department</nav>
+      <main>
+        <!--SHS-SECTIONS-->
+        <div class="floating">
+          <header>
+            <div class="title">
+              <h3>Strand Sections</h3>
+              <small> *insert school year* </small>
+            </div>
+          </header>
+          <main>
+            <table>
+              <thead>
+                <tr>
+                  <th>Strand</th>
+                  <th>Grade 11</th>
+                  <th>Grade 12</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button type="button" class="information">View</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </main>
+        </div>
 
-
-
+        <!--COLLEGE-SECTIONS-->
+        <div class="floating">
+          <header>
+            <div class="title">
+              <h3>Course Sections</h3>
+              <small> *insert school year* </small>
+            </div>
+          </header>
+          <main>
+            <table>
+              <thead>
+                <tr>
+                  <th>Course</th>
+                  <th>1st Year</th>
+                  <th>2nd Year</th>
+                  <th>3rd Year</th>
+                  <th>4th Year</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button type="button" class="information">View</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </main>
+        </div>
+      </main>
+    </div>
