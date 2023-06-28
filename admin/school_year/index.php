a3375ec0749ae02e2e4cb89d942c07f624fc61a5
IrisLibre32 <dhanmanaog@gmail.com>
2023-06-28 11:35:46 +0800

    updated

--- admin/school_year/index.php
+++ admin/school_year/index.php
@@ -2,7 +2,33 @@
 
 <?php include_once('../../includes/admin_header.php')?>
 
-<h4>This is S.Y Index</h4>
-
-
-
+<div class="content">
+      <nav>Department</nav>
+      <main>
+        <div class="floating">
+          <header>
+            <div class="title">
+              <h3>School Year</h3>
+            </div>
+          </header>
+          <main>
+            <table>
+              <tr>
+                <td style="font-weight: 700">2022-2023</td>
+                <td style="text-align: right">Current</td>
+                <td>
+                  <button
+                    type="button"
+                    class="redirect-btn"
+                    id="shs-calendar"
+                    onclick="shs_calendar()"
+                  >
+                    <i class="bi bi-arrow-right-circle"></i>
+                  </button>
+                </td>
+              </tr>
+            </table>
+          </main>
+        </div>
+      </main>
+    </div>
