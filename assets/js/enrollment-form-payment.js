2254c730e9748f46db9e0137982ce1786f0c28d3
IrisLibre32 <dhanmanaog@gmail.com>
2023-07-07 12:46:00 +0800

    updated

---
+++ assets/js/enrollment-form-payment.js
@@ -0,0 +1,30 @@
+var tab1 = document.getElementById("student-details");
+var tab2 = document.getElementById("enrolled-subjects");
+
+var shsDetails = document.getElementById("shs-information");
+var shsEnrollDetails = document.getElementById("shs-enrollment-details");
+var shsStrandSubjects = document.getElementById("shs-strand-subjects");
+
+function Tab1() {
+    tab1.style.background = "var(--mainContentBG)";
+    tab1.style.color = "black";
+    tab2.style.background = "var(--theme)";
+    tab2.style.color = "white";
+
+    shsDetails.style.display = "flex";
+    shsEnrollDetails.style.display = "none";
+    shsStrandSubjects.style.display = "none";
+}
+
+function Tab2() {
+    tab1.style.background = "var(--theme)";
+    tab1.style.color = "white";
+    tab2.style.background = "var(--mainContentBG)";
+    tab2.style.color = "black";
+
+    shsDetails.style.display = "none";
+    shsEnrollDetails.style.display = "flex";
+    shsStrandSubjects.style.display = "flex";
+}
+
+window.onload = Tab1;
