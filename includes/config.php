a4216f6ae622b6062e3c81a4c066a60fb0bfeb7d
buboyskie26 <72244546+buboyskie26@users.noreply.github.com>
2023-09-17 21:03:24 +0800

    com

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,7 @@
     // define('DB_USERNAME', 'root');
     // define('DB_PASSWORD', '');
     // define('DB_DATABASE', 'dcbt');
-    
+
     define('DB_USERNAME', 'u544299924_master');
     define('DB_PASSWORD', 'xTVeEU~k=O8:');
     define('DB_DATABASE', 'u544299924_main');
@@ -20,7 +20,7 @@
         // string that specifies the details of the database connection, including the database driver
         $data_source = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_DATABASE;
         $con = new PDO($data_source, DB_USERNAME, DB_PASSWORD);
-        $con = new PDO('mysql:host=localhost;port=3307;dbname=dcbt', 'root', '');
+        // $con = new PDO('mysql:host=localhost;port=3307;dbname=dcbt', 'root', '');
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
         $this_file = str_replace('\\', '/', __File__) ;
