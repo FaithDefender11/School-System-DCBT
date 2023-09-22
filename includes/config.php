db7bc33dd6ad3e103c176e0fb7f7929562b6df2e
buboyskie26 <72244546+buboyskie26@users.noreply.github.com>
2023-09-22 17:05:44 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,22 +7,22 @@
     define('DB_HOST', 'localhost');
     define('DB_PORT', 3307); // Update with your specific port number
 
-    // define('DB_USERNAME', 'root');
-    // define('DB_PASSWORD', '');
-    // define('DB_DATABASE', 'dcbt');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'dcbt');
 
-    define('DB_USERNAME', 'u544299924_master');
-    define('DB_PASSWORD', 'xTVeEU~k=O8:');
-    define('DB_DATABASE', 'u544299924_main');
+    // define('DB_USERNAME', 'u544299924_master');
+    // define('DB_PASSWORD', 'xTVeEU~k=O8:');
+    // define('DB_DATABASE', 'u544299924_main');
 
     try {
         
         // string that specifies the details of the database connection, including the database driver
         $data_source = "mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_DATABASE;
 
-        $con = new PDO($data_source, DB_USERNAME, DB_PASSWORD);
+        // $con = new PDO($data_source, DB_USERNAME, DB_PASSWORD);
 
-        // $con = new PDO('mysql:host=localhost;port=3307;dbname=dcbt', 'root', '');
+        $con = new PDO('mysql:host=localhost;port=3307;dbname=dcbt', 'root', '');
 
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
