2694afa1b5ebb9abd1e213c6f5cced5c102cdc9d
buboyskie26 <72244546+buboyskie26@users.noreply.github.com>
2023-09-22 15:35:29 +0800

    a

--- enrollee_logout.php
+++ enrollee_logout.php
@@ -7,10 +7,13 @@
 
     session_destroy();
 
-    // $url = LOCAL_BASE_URL . "/pre_enrollment_login.php";
-    $url = 'http://' . $_SERVER['HTTP_HOST'] . "/pre_enrollment_login.php";
+    $url_local = LOCAL_BASE_URL . "/pre_enrollment_login.php";
+    $url_online = 'http://' . $_SERVER['HTTP_HOST'] . "/pre_enrollment_login.php";
+
+    // $url = 'http://' . $_SERVER['HTTP_HOST'] . "/index.php";
+    $url = $_SERVER['SERVER_NAME'] === 'localhost' ? $url_local : $url_online;
+
     header("Location: $url");
-    // header("Location: /school-system-dcbt/pre_enrollment_login.php");
 
 
 ?>
