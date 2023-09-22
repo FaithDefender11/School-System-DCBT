0c5b446334a19c8abbb342e101848649bcf9b527
buboyskie26 <72244546+buboyskie26@users.noreply.github.com>
2023-09-22 16:19:28 +0800

    Update lms_logout.php

--- lms_logout.php
+++ lms_logout.php
@@ -3,22 +3,60 @@ require_once('includes/config.php');
 
 session_start();
 
-$url_student = LOCAL_BASE_URL . "/student_lms.php";
+// $url_local = LOCAL_BASE_URL . "/pre_enrollment_login.php";
+// $url_online = 'http://' . $_SERVER['HTTP_HOST'] . "/pre_enrollment_login.php";
+
+
+$url_student_local = LOCAL_BASE_URL . "/student_lms.php";
+$url_student_online = 'http://' . $_SERVER['HTTP_HOST'] . "/student_lms.php";
+
+$url_users_local = LOCAL_BASE_URL . "/teacher_lms.php";
+$url_users_online = 'http://' . $_SERVER['HTTP_HOST'] . "/teacher_lms.php";
+
 $home = LOCAL_BASE_URL . "/index.php";
-$url_users = LOCAL_BASE_URL . "/teacher_lms.php";
+
+// $url_local = LOCAL_BASE_URL . "/pre_enrollment_login.php";
+// $url_online = 'http://' . $_SERVER['HTTP_HOST'] . "/pre_enrollment_login.php";
+
+// // $url = 'http://' . $_SERVER['HTTP_HOST'] . "/index.php";
+// $url = $_SERVER['SERVER_NAME'] === 'localhost' ? $url_local : $url_online;
+
 
 if (isset($_SESSION['role'])) {
-    if ($_SESSION['role'] === "admin" || $_SESSION['role'] === "teacher") {
-        header("Location: $url_users");
-        exit();
-        
-    } elseif ($_SESSION['role'] === "student") {
-        header("Location: $url_student");
-        exit();
+
+    if($_SERVER['SERVER_NAME'] === 'localhost'){
+        if ($_SESSION['role'] === "admin" || $_SESSION['role'] === "teacher") {
+
+            header("Location: $url_users_local");
+            session_destroy();
+            exit();
+
+        } elseif ($_SESSION['role'] === "student") {
+            header("Location: $url_student_local");
+            session_destroy();
+            exit();
+        }
+    }else{
+        if ($_SESSION['role'] === "admin" || $_SESSION['role'] === "teacher") {
+
+            header("Location: $url_users_online");
+            session_destroy();
+
+            exit();
+
+        } elseif ($_SESSION['role'] === "student") {
+            header("Location: $url_student_online");
+            session_destroy();
+            exit();
+        }
     }
+    
+        
+    
 }
 
-header("Location: $home");
-session_destroy();
-exit();
+
+// header("Location: $home");
+// session_destroy();
+// exit();
 ?>
