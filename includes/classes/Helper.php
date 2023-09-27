776bcfc308ae9d0c71b225468d0979d759e4bf4b
IrisLibre32 <dhanmanaog@gmail.com>
2023-09-27 21:08:07 +0800

    Update Helper.php

--- includes/classes/Helper.php
+++ includes/classes/Helper.php
@@ -1254,7 +1254,7 @@ class Helper {
 
         if($isTertiary == true){
             $buttonTop = "
-                <div id='btn' style='left: 121.5px'></div>
+                <div id='btn' style='left: 122px'></div>
             ";
         }
 
@@ -1301,7 +1301,7 @@ class Helper {
 
         if($isTertiary == true){
             $buttonTop = "
-                <div id='btn' style='left: 121.5px'></div>
+                <div id='btn' style='left: 122px'></div>
             ";
         }
 
