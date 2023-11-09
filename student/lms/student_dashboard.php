24af4aee3643564e3748bea0c3d5450930a1efbd
buboyskie26 <72244546+buboyskie26@users.noreply.github.com>
2023-11-09 08:32:52 +0800

    Update student_dashboard.php

--- student/lms/student_dashboard.php
+++ student/lms/student_dashboard.php
@@ -241,7 +241,7 @@
                     style="background-color: var(--theme); color: white"
                     onclick="window.location.href='completed_subjects.php'">
 
-                    Completed (<?= count($getPreviousEnrolledSubjects)?>)
+                    Completed qweqwe (<?= count($getPreviousEnrolledSubjects)?>)
 
 
             </button>
