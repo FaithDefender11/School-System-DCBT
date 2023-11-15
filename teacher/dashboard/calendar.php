415e81caccbaf37033fd4a736b6b0e730b63e136
IrisLibre32 <dhanmanaog@gmail.com>
2023-11-15 22:57:11 +0800

    Update calendar.php

--- teacher/dashboard/calendar.php
+++ teacher/dashboard/calendar.php
@@ -1,14 +1,9 @@
-
-<?php 
-
+<?php
     include_once('../../includes/teacher_header.php');
     include_once('../../includes/classes/SchoolYear.php');
     include_once('../../includes/classes/SubjectCodeAssignment.php');
     include_once('../../includes/classes/SubjectPeriodCodeTopic.php');
     include_once('../../includes/classes/SubjectAssignmentSubmission.php');
-    
-    echo Helper::RemoveSidebar();
-
 
     $school_year = new SchoolYear($con);
     $school_year_obj = $school_year->GetActiveSchoolYearAndSemester();
@@ -18,162 +13,113 @@
     $current_school_year_term = $school_year_obj['term'];
 
     $teacher_id = $_SESSION['teacherLoggedInId'];
-
 ?>
 
-<!DOCTYPE html>
-<html>
+      <nav>
+        <a href="<?php echo $back_url;?>">
+          <i class="bi bi-arrow-return-left"></i>
+          Back
+        </a>
+      </nav>
 
-  <head>
-
-    <title>My Calendar</title>
-    <!-- *Note: You must have internet connection on your laptop or pc other wise below code is not working -->
-    <!-- CSS for full calender -->
-    <link
-      href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css"
-      rel="stylesheet"
-    />
-    <!-- JS for jQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- JS for full calender -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
-    <!-- bootstrap css and js -->
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    />
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  
-
-    <style>
-
-    .fc-content {
-   width: auto !important;
-    white-space: normal !important;
-    overflow: visible !important;
-    }
-    </style>
-  </head>
-
-<body>
-
-    <div class="row col-md-12">
-
-      <div class="col-md-9">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <br>
-              <h4 style="font-weight: bold;" class="mt-3 text-primary text-start">
-                Given assignments Calendar
-              </h4>
-              <br>
-              <div id="calendar"></div>
+      <main>
+        <div class="floating">
+          <header>
+            <div class="title">
+              <h3>Given Assignments Calendar</h3>
             </div>
-          </div>
+          </header>
+          <main>
+            <div id="calendar"></div>
+          </main>
         </div>
-      </div>
 
-      <div class="col-md-3">
-              <br>
+            <!-- Start popup dialog box -->
+            <!-- <div
+              class="modal fade"
+              id="event_entry_modal"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="modalLabel"
+              aria-hidden="true">
+              <div class="modal-dialog modal-md" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabel">Add New Event</h5>
+                    <button
+                      type="button"
+                      class="close"
+                      data-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      <span aria-hidden="true">×</span>
+                    </button>
+                  </div>
 
-        <h4>Legend</h4>
-      </div>
+                  <div class="modal-body">
+                    <div class="img-container">
+                      <div class="row">
+                        <div class="col-sm-12">
+                          <div class="form-group">
+                            <label for="event_name">Event name</label>
+                            <input
+                              type="text"
+                              name="event_name"
+                              id="event_name"
+                              class="form-control"
+                              placeholder="Enter your event name"
+                            />
+                          </div>
+                        </div>
+                      </div>
 
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <div class="form-group">
+                            <label for="event_start_date">Event start</label>
+                            <input
+                              type="date"
+                              name="event_start_date"
+                              id="event_start_date"
+                              class="form-control onlydatepicker"
+                              placeholder="Event start date"
+                            />
+                          </div>
+                        </div>
+                        <div class="col-sm-6">
+                          <div class="form-group">
+                            <label for="event_end_date">Event end</label>
+                            <input
+                              type="date"
+                              name="event_end_date"
+                              id="event_end_date"
+                              class="form-control"
+                              placeholder="Event end date"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                      onclick="save_event()"
+                    >
+                      Save Event
+                    </button>
+                  </div>
+                </div>
+              </div>
 
+            </div> -->
+
+            <!-- End popup dialog box -->
+      </main>
     </div>
-
-
-    <!-- Start popup dialog box -->
-    <!-- <div
-      class="modal fade"
-      id="event_entry_modal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="modalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog modal-md" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="modalLabel">Add New Event</h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-
-          <div class="modal-body">
-            <div class="img-container">
-              <div class="row">
-                <div class="col-sm-12">
-                  <div class="form-group">
-                    <label for="event_name">Event name</label>
-                    <input
-                      type="text"
-                      name="event_name"
-                      id="event_name"
-                      class="form-control"
-                      placeholder="Enter your event name"
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="event_start_date">Event start</label>
-                    <input
-                      type="date"
-                      name="event_start_date"
-                      id="event_start_date"
-                      class="form-control onlydatepicker"
-                      placeholder="Event start date"
-                    />
-                  </div>
-                </div>
-                <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="event_end_date">Event end</label>
-                    <input
-                      type="date"
-                      name="event_end_date"
-                      id="event_end_date"
-                      class="form-control"
-                      placeholder="Event end date"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-primary"
-              onclick="save_event()"
-            >
-              Save Event
-            </button>
-          </div>
-        </div>
-      </div>
-
-    </div> -->
-
-    <!-- End popup dialog box -->
-
-    <br />
-</body>
-
-<script>
-
-  $(document).ready(function () {
+  <script>
+    $(document).ready(function () {
 
     var teacherId = `
       <?php echo $teacher_id; ?>
@@ -185,9 +131,9 @@
 
     display_events(teacherId, current_school_year_id);
 
-  }); //end document.ready block
+    }); //end document.ready block
 
-  function display_events(teacherId, current_school_year_id) {
+    function display_events(teacherId, current_school_year_id) {
 
     var events = []; // Initialize an empty array to store events
 
@@ -260,45 +206,44 @@
 
       },
     }); // end ajax block
-  }
+    }
 
-  // function save_event(){
+    // function save_event(){
 
-  //     var event_name=$("#event_name").val();
-  //     var event_start_date=$("#event_start_date").val();
-  //     var event_end_date=$("#event_end_date").val();
-  //     if(event_name=="" || event_start_date=="" || event_end_date=="")
-  //     {
-  //     alert("Please enter all required details.");
-  //     return false;
-  //     }
-  //     $.ajax({
-  //     url:"save_event.php",
-  //     type:"POST",
-  //     dataType: 'json',
-  //     data: {event_name:event_name,event_start_date:event_start_date,event_end_date:event_end_date},
-  //     success:function(response){
-  //     $('#event_entry_modal').modal('hide');
-  //     if(response.status == true)
-  //     {
-  //         alert(response.msg);
-  //         location.reload();
-  //     }
-  //     else
-  //     {
-  //         alert(response.msg);
-  //     }
-  //     },
-  //     error: function (xhr, status) {
-  //     console.log('ajax error = ' + xhr.statusText);
-  //     alert(response.msg);
-  //     }
-  //     });
+    //     var event_name=$("#event_name").val();
+    //     var event_start_date=$("#event_start_date").val();
+    //     var event_end_date=$("#event_end_date").val();
+    //     if(event_name=="" || event_start_date=="" || event_end_date=="")
+    //     {
+    //     alert("Please enter all required details.");
+    //     return false;
+    //     }
+    //     $.ajax({
+    //     url:"save_event.php",
+    //     type:"POST",
+    //     dataType: 'json',
+    //     data: {event_name:event_name,event_start_date:event_start_date,event_end_date:event_end_date},
+    //     success:function(response){
+    //     $('#event_entry_modal').modal('hide');
+    //     if(response.status == true)
+    //     {
+    //         alert(response.msg);
+    //         location.reload();
+    //     }
+    //     else
+    //     {
+    //         alert(response.msg);
+    //     }
+    //     },
+    //     error: function (xhr, status) {
+    //     console.log('ajax error = ' + xhr.statusText);
+    //     alert(response.msg);
+    //     }
+    //     });
 
-  //     return false;
+    //     return false;
 
-  // }
-</script>
-
-
+    // }
+  </script>
+  </body>
 </html>
