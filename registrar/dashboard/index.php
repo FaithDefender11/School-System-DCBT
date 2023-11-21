f11898fe1330661740452ca355713f5106f1620b
buboyskie26 <72244546+buboyskie26@users.noreply.github.com>
2023-11-21 11:12:03 +0800

    Update index.php

--- registrar/dashboard/index.php
+++ registrar/dashboard/index.php
@@ -12,21 +12,21 @@
     $current_school_year_period = $school_year_obj['period'];
     $current_school_year_id = $school_year_obj['school_year_id'];
  
-    $processEnrolled = true;
-    $current_enrollment_course_id = 1281;
+    // $processEnrolled = true;
+    // $current_enrollment_course_id = 1281;
 
-    $originalValueIsFalse = false;
+    // $originalValueIsFalse = false;
 
     
-    $enrollment = new Enrollment($con);
+    // $enrollment = new Enrollment($con);
 
-    $check = $enrollment->GetAllRegistrarIdIntheEnrollment(
-        $current_school_year_id, $registrarUserId);
+    // $check = $enrollment->GetAllRegistrarIdIntheEnrollment(
+    //     $current_school_year_id, $registrarUserId);
 
-    $base_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/';
+    // $base_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/';
 
-    $dirname = dirname($_SERVER['PHP_SELF']);
-    $after_registrar = substr($dirname, strpos($dirname, 'registrar/') + strlen('registrar/'));
+    // $dirname = dirname($_SERVER['PHP_SELF']);
+    // $after_registrar = substr($dirname, strpos($dirname, 'registrar/') + strlen('registrar/'));
 
     // var_dump($after_registrar);
     
@@ -169,207 +169,6 @@
             </header>
 
             <main>
-
-                <table id="shs_program_table"
-                    class="a" style="margin: 0;  ">
-                    <thead>
-                        <tr>
-                            <th>Enrollment ID</th>
-                            <th>Registrar Inside</th>
-                            <th>Cashier Inside</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <?php
-
-                            $query = $con->prepare("SELECT t1.* FROM enrollment as t1 
-
-                                WHERE t1.school_year_id = :school_year_id
-                                AND ( t1.currently_registrar_id IS NOT NULL
-                                    OR t1.currently_cashier_id IS NOT NULL
-                                )
-
-                            ");
-
-                            $query->bindValue(":school_year_id", $current_school_year_id);
-                            $query->execute();
-
-                            if($query->rowCount() > 0){
-
-                                while($row = $query->fetch(PDO::FETCH_ASSOC)){
-
-                                    $course_id = $row['course_id'];
-                                    $enrollment_id = $row['enrollment_id'];
-                                    $enrollment_form_id = $row['enrollment_form_id'];
-                                    $school_year_id = $row['school_year_id'];
-                                    $currently_registrar_id = $row['currently_registrar_id'];
-                                    $currently_cashier_id = $row['currently_cashier_id'];
-
-
-                                    $hasUserInIt = "
-                                        <i class='fas fa-times'></i>
-                                    ";
-
-                                    // var_dump($currently_registrar_id);
-                                    // echo "<br>";
-                                    // var_dump($currently_cashier_id);
-                                    // echo "<br>";
-
-
-                                    $section = new Section($con, $course_id);
-                                    $sectionCapacity = $section->GetSectionCapacity();
-
-                                    // $count = $section->GetEnrollmentCourseIdEnrolledCount($course_id, $school_year_id);
-                                    // if($sectionCapacity == $count){
-                                    //     array_push($reachedMaxEnrollmentArr, $row);
-                                    // }
-
-                                    
-                                    $removeCurrentRegistrarIdBtn = "";
-                                    $removeCurrentCashierIdBtn = "";
-
-                                    if($currently_registrar_id != NULL || $currently_cashier_id != NULL){
-
-                                        // echo "currently_registrar_id: not null";
-                                        // echo "<br>";
-                                        // echo "currently_cashier_id: not null";
-                                        // echo "<br>";
-
-                                        $registrarUser = new User($con, $currently_registrar_id);
-                                        $cashierUser = new User($con, $currently_cashier_id);
-
-                                        $removeCurrentRegistrarIdBtn = "removeCurrentRegistrarIdBtn($currently_registrar_id)";
-                                        $removeCurrentCashierIdBtn = "removeCurrentCashierIdBtn($currently_cashier_id)";
-
-                                        $registName = ucwords($registrarUser->getFirstName()) . " " . ucwords($registrarUser->getLastName());
-                                        $cashierName = ucwords($cashierUser->getFirstName()) . " " . ucwords($cashierUser->getLastName());
-
-                                        // $hasUserInIt = "
-                                        //     <i class='fas fa-check'></i>
-                                        // ";
-
-                                    }else{
-                                        $cashierName = "N/A";
-                                        $registName = "N/A";
-                                    }
-
-                                    // else if($currently_registrar_id == NULL 
-                                    //     || $currently_cashier_id == NULL){
-
-                                    //     echo "currently_registrar_id:  null";
-                                    //     echo "<br>";
-                                    //     echo "currently_cashier_id:  null";
-                                    //     echo "<br>";
-                                    // }
-
-                                    var_dump($registName);
-                                    var_dump($cashierName);
-
-
-                                    echo "
-                                        <tr>
-                                            <td>$enrollment_form_id</td>
-                                            <td>
-                                                <a onclick='$removeCurrentRegistrarIdBtn' style='color: gray' href='#'>
-                                                    $registName
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <a onclick='$removeCurrentCashierIdBtn' style='color: gray' href='#'>
-                                                        $cashierName
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <a href=''>
-                                                    <button class='btn-sm btn btn-primary'>
-                                                        <i class='fas fa-pencil'></i>
-                                                    </button>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    ";
-
-                                }
-                            }
-
-                           
-
-                        ?>
-                    </tbody>
-                </table>
-
-
-
-                <table id="shs_program_table"
-                    class="a" style="margin: 0; display: none">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Applying for</th>
-                            <th>Maximum</th>
-                            <th>Enrolled</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <?php
-
-                            foreach ($reachedMaxEnrollmentArr as $key => $row) {
-
-                                $db_student_id = $row['student_id'];
-
-                                $student = new Student($con, $db_student_id);
-
-                                $db_course_id = $row['course_id'];
-                                $db_enrollment_id = $row['enrollment_id'];
-                                $db_enrollment_form_id= $row['enrollment_form_id'];
-                                $db_school_year_id= $row['school_year_id'];
-
-                                $section = new Section($con, $db_course_id);
-
-                                $programSection = $section->GetSectionName();
-                                $capacity = $section->GetSectionCapacity();
-
-                                $studentName = ucwords($student->GetFirstName()) . " " . ucwords($student->GetLastName());
-                                $studentUniqueId = $student->GetStudentUniqueId();
-
-
-                                // $removeProgramBtn = "removeProgramBtn($program_id)";
-
-                                $url = "../admission/subject_insertion_summary.php?id=$db_enrollment_id&enrolled_subject=show";
-                                
-                                $count = $section->GetTotalNumberOfStudentInSection($db_course_id, $db_school_year_id);
-                                
-                                echo "
-
-                                    <tr>
-                                        
-                                        <td>$db_enrollment_form_id</td>
-                                        <td>$studentName</td>
-                                        <td>$programSection</td>
-                                        <td>$capacity</td>
-                                        <td>$count</td>
-                                        
-                                        <td>
-                                            <a href='$url'>
-                                                <button class='btn-sm btn btn-primary'>
-                                                    <i class='fas fa-eye'></i>
-                                                </button>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                
-                                ";
-                            }
-
-                        ?>
-                    </tbody>
-                </table>
-
 
 
             </main>
